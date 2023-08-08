abff74e6d0f36d5944d463592fc5f7c2f589e23d
WebHav0cx <adekanbisamuel20023@gmail.com>
2023-08-08 16:06:19 +0100

    changes from browser router to hash roteru beacuse of 404 error

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import Login from "./Auth/Login";
 import Signup from "./Auth/Signup";
