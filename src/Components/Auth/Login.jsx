45443fc79362dcf6f8cac2ae2fb4f6ea2ab3917b
WebHav0cx <adekanbisamuel20023@gmail.com>
2023-08-08 14:59:50 +0100

    added login page

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,9 +1,52 @@
 import React from "react";
+import Logo from "../../assets/Logo.png";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <h2>hello</h2>
+    <div className="flex justify-center items-center">
+      <section className="bg-default-green w-[37.4rem] h-[41rem] ">
+        <div className="ml-[0.06rem] w-[35.5rem] h-[39rem] bg-white rounded-tl-[6.875rem] rounded-br-[6.875rem] flex flex-col gap-16 ">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <img className="w-28 h-20" src={Logo} alt="Logo" />
+
+            <div className="flex flex-col justify-center items-center gap-2">
+              <h2 className="text-4xl text-default-text">Sign In</h2>
+              <p className="text-xs">Sign in to your account</p>
+            </div>
+          </div>
+          <form className="flex flex-col gap-4 max-w-lg px-12">
+            <input
+              type="email"
+              className="border border-default-input rounded-lg py-2 px-2 bg-input-background"
+              placeholder="Email Address"
+              required
+            />
+            <input
+              type="password"
+              className="border border-default-input rounded-lg py-2 px-2 bg-input-background"
+              placeholder="Password"
+              required
+            />
+
+            <a href="/" className="text-end text-red-600">
+              Forgot Password?
+            </a>
+            <button
+              className="bg-default-green text-white py-2 rounded-md border border-yellow-fine "
+              type="submit"
+            >
+              Sign In
+            </button>
+            <p className="mt-4">
+              If you don't have Account with us{" "}
+              <span className="font-semibold">
+                <Link to="/signup">Signup</Link>
+              </span>
+            </p>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         "button-default": "#154735",
         "deal-default": "#1D6E10",
         "default-orange": "#DE8838",
+        "input-background": "#DFE7DC",
       },
       colors: {
         "header-p": "rgba(255, 255, 255, 0.75)",
@@ -18,6 +19,7 @@ export default {
         "yellow-fine": "#FFCA28;",
         "default-green": "#1D6E10",
         "input-green-border": "rgba(22, 129, 20, 0.50)",
+        "default-input": "#215626",
       },
 
       boxShadow: {
